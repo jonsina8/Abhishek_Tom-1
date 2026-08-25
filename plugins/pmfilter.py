b0021e77cb90900016c4029d6cd86433677d006a
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-08-25 10:56:44 +0530

    Refactor inline keyboard button arrangement
    Reorganized inline keyboard buttons for better layout.

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1170,14 +1170,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data="about"),
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                ],[
-                    InlineKeyboardButton('ᴇᴀʀɴ ᴍᴏɴᴇʏ..💵', callback_data="shortlink_info")
-                    InlineKeyboardButton(' CHANNEL 📖', url=CHNL_LNK)
-                ],[
-                    InlineKeyboardButton(' GROUPS 📢', url=GRP_LNK)
-                ]]
+                        InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
+                        InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                    ],[
+                        InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
+                        InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
         curr_time = current_time.hour
