aaf92a8468bc081fae4afacfb1250a6346073790
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-08-29 12:17:20 +0530

    Fix formatting of subscription button in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1174,7 +1174,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         InlineKeyboardButton('❗ ABOUT', callback_data='about')
                     ],[
                         InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', callback_data="topsearch"),
-                        InlineKeyboardButton(' 𝐟𝐫𝐞𝐞/𝐩𝐚𝐢𝐝 𝐒𝐔𝐁𝐒𝐂𝐑𝐈𝐏𝐓𝐈𝐎𝐍😊', callback_data="premium_info"),
+                        InlineKeyboardButton('𝐟𝐫𝐞𝐞/𝐩𝐚𝐢𝐝 𝐒𝐔𝐁𝐒𝐂𝐑𝐈𝐏𝐓𝐈𝐎𝐍😊', callback_data="premium_info"),
                     ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
